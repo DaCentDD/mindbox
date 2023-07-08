a550cf97ae529b253cf66ccd2eb28fe368c1c525
Roman Vasiliev <r.vasiliev@company.dnevnik.ru>
2023-07-08 18:41:22 +0300

    Вопрос 1

--- MB/Calculators/TriangleCalculator.cs
+++ MB/Calculators/TriangleCalculator.cs
@@ -1,6 +1,5 @@
 ﻿using MB.Calculators.Interfaces;
 using MB.Models;
-using MB.Models.Interfaces;
 
 namespace MB.Calculators;
 
@@ -18,21 +17,7 @@ internal class TriangleCalculator : ICalculator
 
     public double Calculate()
     {
-        if (IsTriangleRight())
-        {
-            return (FirstLeg * SecondLeg) / 2;
-        }
-
         var halfPerimeter = (FirstLeg + SecondLeg + Hypotenuse) / 2;
         return Math.Sqrt(halfPerimeter * (halfPerimeter - FirstLeg) * (halfPerimeter - SecondLeg) * (halfPerimeter - Hypotenuse));
-    }
-
-    private bool IsTriangleRight()
-    {
-        var hypotenuseSquared = Math.Round(Math.Pow(Hypotenuse, 2));
-        var firstLegSquared = Math.Round(Math.Pow(FirstLeg, 2));
-        var secondLegSquared = Math.Round(Math.Pow(SecondLeg, 2));
-
-        return Math.Abs(firstLegSquared + secondLegSquared - hypotenuseSquared) < double.Epsilon;
     }
 }
